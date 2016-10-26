2fca27fd24977ccd5610a9e27cba73cbf5dd8fa0
Tom Elliott <tomelliottnz@gmail.com>
2016-10-26 14:33:26 +1300

    add start time field

--- gtfsrdb.py
+++ gtfsrdb.py
@@ -277,7 +277,7 @@ try:
                     dbvp = VehiclePosition(
                         trip_id = vp.trip.trip_id,
                         route_id = vp.trip.route_id,
-                        # trip_start_time = vp.trip.start_time,
+                        trip_start_time = vp.trip.start_time,
                         # trip_start_date = vp.trip.start_date,
                         # vehicle_id = vp.vehicle.id,
                         # vehicle_label = vp.vehicle.label,
